89bb10c22bb8b1791b7c792fe5038fa15fb64115
Kadir Tereci <kadir.tereci@gmail.com>
2022-01-02 23:32:32 +0300

    event comments added

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+//image to drag
 const imgBox = document.querySelector(".imgBox");
+
+//droppable containers
 const containers = document.getElementsByClassName("container");
 
+//It is fired when the user starts dragging an item.
 imgBox.addEventListener("dragstart", (e) => {
   console.log("DragStart triggered");
   e.target.className += " hold";
@@ -9,32 +13,34 @@ imgBox.addEventListener("dragstart", (e) => {
   }, 0);
 });
 
+//It is fired when a drag operation ends.
 imgBox.addEventListener("dragend", (e) => {
   console.log("DragEnd triggered");
   e.target.className = "imgBox";
 });
 
 for (container of containers) {
+  //It is fired when a dragged item is being dragged over a valid drop target.
   container.addEventListener("dragover", (e) => {
     e.preventDefault();
     console.log("DragOver triggered");
   });
 
+  //It is fired when a dragged item enters a valid drop target.
   container.addEventListener("dragenter", (e) => {
     console.log("DragEnter triggered");
     e.target.className += " dashed";
   });
 
+  //It is fired when a dragged item leaves a valid drop target.
   container.addEventListener("dragleave", (e) => {
     console.log("DragLeave triggered");
     e.target.className = "container";
-    
   });
 
+  //It is fired when an item is dropped on a valid drop target.
   container.addEventListener("drop", (e) => {
     console.log("Drop triggered");
     e.target.append(imgBox);
   });
 }
-
-
